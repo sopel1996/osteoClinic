0f82164192f747a513c5625553984c0f4f3e718c
sopel1996 <sopel@live.ru>
2022-07-15 11:30:30 +0300

    add new page

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,24 @@
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
-    slidesPerView: 5,
+    slidesPerView: 2,
     loop: false,
-  });
+    transitionDuration: "12000ms"
+});
+
+      var swiper1 = new Swiper(".mySwiper", {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        freeMode: true,
+        watchSlidesProgress: true,
+      });
+      var swiper2 = new Swiper(".mySwiper2", {
+        spaceBetween: 10,
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+        thumbs: {
+          swiper: swiper1,
+        },
+      });
